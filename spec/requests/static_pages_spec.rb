554d6ee7de8e511eca19fdc6ef96a909ad34dcef
JerryEipers <jezzer531@yahoo.com>
2014-09-20 14:22:06 -0700

    exercise 5.6.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,7 +29,7 @@ describe "StaticPages" do
 	describe "Contact Page" do
 		before { visit contact_path}
 
-		it { should have_content('Contact Us')}
+		it { should have_selector('h1', text:'Contact')}
 		it { should have_title(full_title('Contact'))}
 	end
 
