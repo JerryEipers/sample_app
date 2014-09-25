fa564990751b245335f3e756aafdfaa0c7936686
JerryEipers <jezzer531@yahoo.com>
2014-09-24 21:28:17 -0700

    Chap 9 exercises, except #6 and #9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,8 @@ describe "Authentication" do
 
     	it { should have_content('Sign in')}
     	it { should have_title('Sign in')}
+    	it { should_not have_link('Profile')}
+    	it { should_not have_link('Settings')}
   	end
 
 	describe "signin" do
@@ -53,9 +55,7 @@ describe "Authentication" do
 			describe "when attempting to visit a protected page" do
 				before do
 					visit edit_user_path(user)
-					fill_in "Email",		with: user.email
-					fill_in "Password",		with: user.password
-					click_button "Sign in"
+					sign_in user
 				end
 
 				describe "after signing in" do
@@ -63,6 +63,19 @@ describe "Authentication" do
 					it "should render the desired protected page" do
 						expect(page).to have_title('Edit user')
 					end
+				
+					describe "when signing in again" do
+						before do
+							click_link "Sign out"
+							visit signin_path
+							sign_in user
+						end
+
+						it "should render the default (profile) page" do
+							expect(page).to have_title(user.name)
+						end
+					end
+
 				end
 			end
 
